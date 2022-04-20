a9abf3f078f2fd191e4d9915f5c7cabe0ac591b7
Vincent <mvincentbb@gmail.com>
2022-04-20 08:54:22 +0200

    header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,24 @@
 import React  from "react";
 import './header.css'
+import CTA from "./CTA";
+import HeaderSocials from "./HeaderSocials";
+import ME from '../../assets/me.png';
 
  const Header = () =>{
     return(
-<header></header>
+        <header>
+            <div className="container header__container">
+                <h5>Hello I'm </h5>
+                <h1>Vincent Bak</h1>
+                <h5 className="text-light"> Fullstack Developer</h5>
+                <CTA/>
+                <HeaderSocials/>
+                <div className="me">
+                    <img src= {ME} alt="me"/>
+                </div>
+
+            </div>
+        </header>
     )
 }
 
